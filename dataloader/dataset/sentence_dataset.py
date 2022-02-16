3987d8eb56adbcfca3e3e724f92e7cc77f3f6f87
sajith.rahim <sajith.rahim@outlook.com>
2022-02-16 15:09:06 +0530

    Create sentence_dataset.py

---
+++ dataloader/dataset/sentence_dataset.py
@@ -0,0 +1,20 @@
+
+from torch.utils.data import Dataset
+from base import BaseDataset
+import torch
+
+
+
+class SentenceDataset(Dataset):
+
+    def __init__(self, data):
+        self.data = data
+
+
+    def __getitem__(self, i: int):
+        return torch.LongTensor(self.data['sents'][i]), \
+               torch.LongTensor([self.data['words_per_sentence'][i]]), \
+               torch.LongTensor([self.data['labels'][i]])
+
+    def __len__(self) -> int:
+        return len(self.data['labels'])
